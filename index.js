f966254a4bda0a5fcea0c9c32087eb0eae8be1de
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2016-11-24 21:55:17 +0300

    Add stub for config-content

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* jshint node: true */
 'use strict';
 
+const getConfig = (config) => `<script>
+  (function() {
+    // put APP-config here
+  })();
+</script>`;
+
 module.exports = {
-  name: 'config-to-html'
+  name: 'config-to-html',
+
+  contentFor(type, config) {
+    if (type === 'config') {
+      return getConfig(config);
+    }
+  }
 };
